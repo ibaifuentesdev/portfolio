22ae3fb6b4a5e9d9c7ba6afc48b8d1fa3f71751c
ibaifuentesdev <ibaifuentes.dev@gmail.com>
2026-03-02 00:50:07 +0100

    *contacto

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -18,4 +18,41 @@ export class ContactComponent {
       href: 'https://www.linkedin.com/in/ibai-fuentes-palacios-92b043208/'
     }
   ];
+
+  protected submissionStatus: 'idle' | 'loading' | 'success' | 'error' = 'idle';
+
+  async onSubmit(event: Event): Promise<void> {
+    event.preventDefault();
+    this.submissionStatus = 'loading';
+
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify({
+      ...object,
+      access_key: '81efe5cb-5ee8-4694-9da4-64fb4bb904bd' // Reemplazar con la clave de web3forms.com
+    });
+
+    try {
+      const response = await fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: json
+      });
+
+      const result = await response.json();
+      if (result.success) {
+        this.submissionStatus = 'success';
+        form.reset();
+      } else {
+        this.submissionStatus = 'error';
+      }
+    } catch (error) {
+      console.error('Error al enviar el formulario:', error);
+      this.submissionStatus = 'error';
+    }
+  }
 }
